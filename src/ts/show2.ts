995ecba7ebc5f65d7dcaf706ba51ef03245970f2
Jack Lee <jack.li@hinterland.tech>
2018-06-01 17:47:48 +0800

    5

--- src/ts/show2.ts
+++ src/ts/show2.ts
@@ -1,3 +1,13 @@
 export function show2 (label:number){
-    console.log(label * 2);
+    let r1 = new XMLHttpRequest();
+    r1.open('GET',"http://localhost:3000/addUser",true);
+    r1.send();
+    r1.onreadystatechange = ()=> {
+        (r1.readyState == 4&&r1.status == 200)?console.log(r1.responseText):console.log(r1.status);
+    }
+    
+    fetch("http://localhost:3000/addUser",{mode:"no-cors"})
+    .then(response => {
+        console.log(response);
+    })
 } 
